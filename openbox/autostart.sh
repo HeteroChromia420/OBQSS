50e0ac2e2bc45f6f0bbdbdf5172e9c0c68f47369
heterochromia420 <56137935+heterochromia420@users.noreply.github.com>
2019-10-03 23:02:43 +0100

    First Release

---
+++ openbox/autostart.sh
@@ -0,0 +1,8 @@
+nitrogen --restore &
+tint2 &
+compton &
+pnmixer &
+package-update-indicator &
+cmst &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
---
+++ quickstart.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+clear
+
+echo "Welcome to NiGHTLY'S OpenBox QuickStart Script! Press Any Key To Continue."
+read -n 1 -s
+echo "This Script will Set Up, For the User, a basic openbox session and configuration for said session."
+echo "Here's The Shortcuts that will be set up for the user:"
+echo "Meta(Windows key)+Up -> Increases Volume"
+echo "Meta+Down -> Decreases Volume"
+echo "PrintScreen -> Runs Screenshot Tool"
+echo "Meta+E -> Opens File Manager"
+echo "Ctrl+Alt+T -> Opens Terminal"
+echo "Ctrl+Alt+Delete -> Opens a Task Manager"
+echo "You can edit any of the shortcuts by editing ~/.config/openbox/rc.xml"
+echo "Press Any Key to continue."
+read -n 1 -s
+echo "This Currently only supports Linux Distros That use Apt as their package manager. Other Users will have to obtain their packages manually."
+echo "If you are running Apt on your system, or don't mind fetching the packages for yourself, press any key to continue, otherwise press ctrl+c  to abort this script."
+read -n 1 -s
+clear
+echo "now, you will recieve a password prompt to run the package manager, it will install some packages for you."
+sudo apt install -y openbox obmenu tint2 arc-theme papirus-icon-theme nitrogen compton pnmixer package-update-indicator cmst policykit-1-gnome xinit firefox-esr slim thunar mousepad lxterminal xfce4-screenshooter
+clear
+echo "All of the packages have been installed."
+echo "now moving all of the configuration files included with this script into their proper places."
+echo "press any key to continue."
+read -n 1 -s
+mkdir ~/.config/openbox
+mv openbox/* ~/.config/openbox
+echo "done! all you have to do is to restart your computer and you should have a usable openbox system!"
+echo "thank you for using this script."
+echo "press any key to exit"
+read -n 1 -s
+exit 

55fd94cb9c51f6aa2db66bad326180a53b1fa5c2
heterochromia420 <56137935+heterochromia420@users.noreply.github.com>
2019-11-07 22:27:09 +0000

    Corrected the messages

--- remove.sh
+++ remove.sh
@@ -7,11 +7,11 @@ read -n 1 -s
 echo "This Script will Remove, For the User, the basic openbox session and configuration for said session set up by the quickstart script"
 echo "Press Any Key to continue."
 read -n 1 -s
-echo "This Currently only supports Linux Distros That use Apt as their package manager. Other Users will have to obtain their packages manually."
-echo "If you are running Apt on your system, or don't mind fetching the packages for yourself, press any key to continue, otherwise press ctrl+c  to abort this script."
+echo "This Currently only supports Linux Distros That use Apt as their package manager. Other Users will have to remove their packages manually."
+echo "If you are running Apt on your system, or don't mind removing the packages for yourself, press any key to continue, otherwise press ctrl+c  to abort this script."
 read -n 1 -s
 clear
-echo "now, you will recieve a password prompt to run the package manager, it will install some packages for you."
+echo "now, you will recieve a password prompt to run the package manager, it will remove some packages for you."
 sudo apt purge --auto-remove -y openbox obmenu tint2 arc-theme papirus-icon-theme nitrogen compton pnmixer package-update-indicator cmst policykit-1-gnome xinit firefox-esr slim pcmanfm scite lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils dunst pavucontrol file-roller smplayer ristretto htop smtube
 clear
 echo "All of the packages have been removed."

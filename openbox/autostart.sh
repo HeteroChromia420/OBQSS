59fbaa46515e7bc17ce45f071323d662454228c7
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-04-27 00:58:17 +0100

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,10 +1,12 @@
+pulseaudio --start &
 nitrogen --restore &
 tint2 &
 compton &
 dunst &
-pnmixer &
 package-update-indicator &
 sleep 5 && cmst &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 kdeconnect-indicator &
 conky &
+xscreensaver --no-splash &
+pnmixer &

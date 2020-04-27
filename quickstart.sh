ed306752faa1558c2b85e76fa74776360e45e364
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-04-27 11:53:48 +0100

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -21,11 +21,10 @@ echo "If you are running Apt on your system, or don't mind fetching the packages
 read -n 1 -s
 clear
 echo "now, you will recieve a password prompt to run the package manager, it will install some packages for you."
-sudo dpkg --add-architecture i386
 sudo apt update
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
 sudo apt-get install  lxsession-logout policykit-1-gnome
-sudo apt-get install  fonts-noto* xscreensaver* gvfs* openbox tint2 arc-theme papirus-icon-theme nitrogen compton pnmixer package-update-indicator cmst xinit firefox-esr slim pcmanfm scite lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils dunst pavucontrol file-roller ristretto htop steam conky gmrun driconf kdeconnect oxygencursors
+sudo apt-get install  fonts-noto* xscreensaver* gvfs* openbox tint2 arc-theme papirus-icon-theme nitrogen compton pnmixer package-update-indicator cmst xinit firefox-esr slim pcmanfm scite lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils dunst pavucontrol file-roller ristretto htop conky gmrun driconf kdeconnect oxygencursors
 echo "All of the packages have been installed."
 echo "now moving all of the configuration files included with this script into their proper places."
 echo "if you see a few "file exists" messages, you can ignore them. it's the script making sure the folders exist."
@@ -39,6 +38,7 @@ cp .conkyrc-lua ~/
 echo "done! all you have to do is to restart your computer and you should have a usable openbox system!"
 echo "to open the menu, right-click on the task bar."
 echo "thank you for using this script."
+echo "also check https://github.com/trizen/obmenu-generator/releases for a better openbox menu!"
 echo "press any key to exit"
 read -n 1 -s
 exit 

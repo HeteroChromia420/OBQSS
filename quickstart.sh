2e7531b781d1773004b94716456db3c9a67ffabb
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-04-27 12:37:28 +0100

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -18,7 +18,7 @@ echo "Press Any Key to continue."
 read -n 1 -s
 echo "This Currently only supports Linux Distros That use Apt as their package manager. Other Users will have to obtain their packages manually."
 echo "If you are running Apt on your system, or don't mind fetching the packages for yourself, press any key to continue, otherwise press ctrl+c  to abort this script."
-echo "if you're running devuan, you'll need to install libpam-elogind before running this script. if you aren't sure, run sudo apt install libpam-elogind before running this script."
+echo "if you're running devuan, you'll need to install elogind before running this script. if you aren't sure, run sudo apt install libpam-elogind before running this script."
 read -n 1 -s
 clear
 echo "now, you will recieve a password prompt to run the package manager, it will install some packages for you."
@@ -26,7 +26,7 @@ sudo apt update
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
 sudo apt-get install lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends
 clear
-echo "if you're running devuan, please press ctrl+c here if this is the first time running the script and reinstall libpam-elogind, otherwise press any key to continue."
+echo "if you're running devuan, please press ctrl+c here if this is the first time running the script and reinstall elogind, otherwise press any key to continue."
 read -n 1 -s
 sudo apt-get install  fonts-noto* xscreensaver* gvfs* openbox tint2 arc-theme papirus-icon-theme nitrogen compton pnmixer package-update-indicator cmst xinit firefox-esr slim pcmanfm scite lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils dunst pavucontrol file-roller ristretto htop conky gmrun driconf kdeconnect oxygencursors
 echo "All of the packages have been installed."

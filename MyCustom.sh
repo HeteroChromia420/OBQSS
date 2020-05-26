1d58afc571a4cd4287001ee3962b9939404a239f
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-05-26 18:03:51 +0100

    Update MyCustom.sh

--- MyCustom.sh
+++ MyCustom.sh
@@ -1,3 +1,6 @@
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo apt-fast install fonts-noto* xscreensaver* arc-theme papirus-icon-theme driconf kdeconnect oxygencursors
 mv openbox/custom.sh ~/.config/openbox/startup.sh
+echo "Done.Press any key to exit..."
+read -n 1 -s
+exit 

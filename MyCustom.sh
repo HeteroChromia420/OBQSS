918bb6ae3f8a562867d3b86942d9a695e473d98d
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-05-26 18:00:58 +0100

    Create MyCustom.sh

---
+++ MyCustom.sh
@@ -0,0 +1,2 @@
+# in this sh file, there's just a bunch of packages that i personally use. you may need to edit your startup.sh to use them.
+sudo apt-fast install fonts-noto* xscreensaver* arc-theme papirus-icon-theme driconf kdeconnect oxygencursors

b1ba3bf426cf6759fd86879bf4d37a709e1f3678
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-05-26 18:12:17 +0100

    Update custom.sh

--- openbox/custom.sh
+++ openbox/custom.sh
@@ -1,3 +1,8 @@
+pulseaudio --start &
+nitrogen --restore &
+tint2 &
+compton &
+dunst &
 package-update-indicator &
 sleep 5 && cmst &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &

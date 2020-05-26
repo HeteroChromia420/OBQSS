ef07fb6dfc7f4862a67f3d7c7b15699bfa1732e8
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-05-26 18:11:32 +0100

    Update custom.sh

--- openbox/custom.sh
+++ openbox/custom.sh
@@ -1,1 +1,7 @@
-
+package-update-indicator &
+sleep 5 && cmst &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+kdeconnect-indicator &
+conky &
+xscreensaver -no-splash &
+pnmixer &

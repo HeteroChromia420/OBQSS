c07132d13986795f39059f446946fc2a2795a34d
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-06-01 18:54:41 +0100

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -2,7 +2,6 @@ pulseaudio --start &
 nitrogen --restore &
 xfce4-panel &
 compton &
-dunst &
 package-update-indicator &
 sleep 5 && cmst &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &

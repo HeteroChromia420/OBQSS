4df448aa64d626417adda76ef239df2b6fb6379f
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-06-01 19:00:21 +0100

    Update MyCustom.sh

--- MyCustom.sh
+++ MyCustom.sh
@@ -6,7 +6,8 @@ clear
 sudo dpkg --add-architecture i386
 sudo apt update
 sudo apt-fast install fonts-noto* xscreensaver* arc-theme papirus-icon-theme driconf kdeconnect oxygencursors steam plank lightdm-gtk-greeter-settings
-mv .custom.sh ~/.config/openbox/startup.sh
+rm ~/.config/openbox/startup.sh
+cp .custom.sh ~/.config/openbox/startup.sh
 echo "Done.Press any key to exit..."
 read -n 1 -s
 exit 

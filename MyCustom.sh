be1b6440c8376ceb3fd2d533067e91c666801256
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-06-22 23:01:21 +0100

    Update MyCustom.sh

--- MyCustom.sh
+++ MyCustom.sh
@@ -8,6 +8,7 @@ sudo apt update
 sudo apt-fast install fonts-noto* xscreensaver* arc-theme papirus-icon-theme kdeconnect oxygencursors steam plank lightdm-gtk-greeter-settings caffeine conky
 rm ~/.config/openbox/autostart.sh
 cp .custom.sh ~/.config/openbox/autostart.sh
+cp -r xfce4_custom ~/.config
 echo "Done.Press any key to exit..."
 read -n 1 -s
 exit 

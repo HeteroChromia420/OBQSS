c4a88e3788c411df6a68ca312d23af399a2915a5
Gillian <56137935+IncognitoChromia420@users.noreply.github.com>
2020-06-25 17:37:20 +0100

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,7 +1,11 @@
 pulseaudio --start &
 nitrogen --restore &
-xfce4-panel &
+tint2 &
 compton &
 package-update-indicator &
 sleep 5 && cmst &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+pnmixer &
+sleep 30 && killall pnmixer
+pnmixer &
+#workaround to get pnmixer to work with pulseaudio

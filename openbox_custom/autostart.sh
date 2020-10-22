6743d4d2df2bbe818dd34f8d2e310a86f1a391b5
Inspector Gilbert <56137935+InspectorGilbert@users.noreply.github.com>
2020-10-22 13:42:27 +0100

    Overhauling time!

---
+++ openbox_custom/autostart.sh
@@ -0,0 +1,13 @@
+pulseaudio --start &
+nitrogen --restore &
+xfce4-panel &
+compton &
+package-update-indicator &
+connman-ui-gtk &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+kdeconnect-indicator &
+conky &
+plank &
+caffeine-indicator &
+xscreensaver -nosplash &
+redshift-gtk &

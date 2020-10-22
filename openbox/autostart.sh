c13c9ef6e07b299ca5b8fb99b70795e0797c2a31
Inspector Gilbert <56137935+InspectorGilbert@users.noreply.github.com>
2020-10-22 08:53:59 +0100

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -3,6 +3,6 @@ nitrogen --restore &
 tint2 &
 compton &
 package-update-indicator &
-sleep 5 && cmst &
+connman-ui-gtk &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 pnmixer &

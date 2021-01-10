7196ca46400d4ac66f979ef06587b9f44791bcad
Professor Gil <56137935+GilProfessor@users.noreply.github.com>
2021-01-10 02:13:55 +0000

    Remove Ungoogled Chromium Repo

--- custom-min.sh
+++ custom-min.sh
@@ -5,7 +5,7 @@ clear
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-fast install gpg xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver nheko xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager -y --no-install-recommends --no-install-suggests
+sudo apt-fast install xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver nheko xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect papirus-icon-theme breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager -y --no-install-recommends --no-install-suggests
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r openbox_custom/* ~/.config/openbox
@@ -13,9 +13,6 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install adriconf
 sudo apt-fast install -y openvpn dialog python3-pip python3-setuptools -y --no-install-recommends --no-install-suggests
 sudo pip3 install protonvpn-cli
-echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
-curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
 sudo apt update
-sudo apt-fast install -y --no-install-recommends --no-install-suggests ungoogled-chromium ungoogled-chromium-sandbox
-sudo apt purge --auto-remove firefox-esr tint2 pnmixer -y
+sudo apt purge --auto-remove tint2 pnmixer -y
 wget -qO- https://git.io/papirus-folders-install | sh

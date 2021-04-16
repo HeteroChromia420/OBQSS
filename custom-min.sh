e24ad4779a779caf98ff1ca98899bb838f3e4113
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-04-16 05:32:01 +0100

    Replace Audacious with Cantata, because mpd

--- custom-min.sh
+++ custom-min.sh
@@ -5,7 +5,7 @@ clear
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-fast install gimp xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver nheko xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect numix-icon-theme-circle breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager xfce4-notifyd -y --no-install-recommends --no-install-suggests 
+sudo apt-fast install cantata mpd gimp xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect numix-icon-theme-circle breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager xfce4-notifyd -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r openbox_custom/* ~/.config/openbox
@@ -14,6 +14,6 @@ sudo flatpak install adriconf
 sudo apt-fast install -y openvpn dialog python3-pip python3-setuptools -y --no-install-recommends --no-install-suggests
 sudo pip3 install protonvpn-cli
 sudo apt update
-sudo apt purge --auto-remove tint2 pnmixer dunst -y
+sudo apt purge --auto-remove tint2 pnmixer dunst audacious -y
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
 git clone https://github.com/numixproject/numix-folders

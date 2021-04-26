7ae5a9b36d1e84480e63ebb11b9a0acf4a8bab1c
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-04-26 15:55:09 +0100

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -8,10 +8,11 @@ sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-in
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
 sudo apt-fast install lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
 clear
-sudo apt-fast install lightdm-gtk-greeter-settings tumbler smplayer audacious mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox tint2 pnmixer nitrogen compton package-update-indicator cmst xinit firefox-esr pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance lxtask pulseaudio alsa-utils dunst pavucontrol file-roller ristretto gmrun xserver-xorg xdg-user-dirs wpasupplicant --no-install-recommends --no-install-suggests -y
+sudo apt-fast install lightdm-gtk-greeter-settings tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox tint2 pnmixer nitrogen compton package-update-indicator cmst xinit firefox-esr pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance lxtask pulseaudio alsa-utils dunst pavucontrol file-roller ristretto gmrun xserver-xorg xdg-user-dirs wpasupplicant --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
 mkdir ~/.config
 mkdir ~/.config/openbox
 cp openbox/* ~/.config/openbox
 cp .conkyrc ~/
 cp .conkyrc-lua ~/
+cp .mpdconf ~/

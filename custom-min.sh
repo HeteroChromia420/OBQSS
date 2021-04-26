97cd141a714e425a212c2d510b5535e4be3a1e41
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-04-26 15:54:33 +0100

    Update custom-min.sh

--- custom-min.sh
+++ custom-min.sh
@@ -5,7 +5,7 @@ clear
 # in this sh file, there's just a bunch of packages that i personally use.
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt-fast install cantata mpd gimp xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect numix-icon-theme-circle breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager xfce4-notifyd -y --no-install-recommends --no-install-suggests 
+sudo apt-fast install desktop-base cantata gimp xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xscreensaver xserver-xorg-video-intel firmware-amd-graphics fonts-noto* redshift-gtk geoclue-2.0 arc-theme kdeconnect numix-icon-theme-circle breeze-cursor-theme steam caffeine conky flatpak plank xfce4-power-manager xfce4-notifyd -y --no-install-recommends --no-install-suggests 
 rm -rf ~/.config/openbox/*
 mkdir ~/.config/openbox
 cp -r openbox_custom/* ~/.config/openbox
@@ -16,5 +16,4 @@ sudo pip3 install protonvpn-cli
 sudo apt update
 sudo apt purge --auto-remove tint2 pnmixer dunst audacious -y
 sudo cp 50-mouse-acceleration.conf /usr/share/X11/xorg.conf.d/
-cp .mpdconf ~/
 git clone https://github.com/numixproject/numix-folders

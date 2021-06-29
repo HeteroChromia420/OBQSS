41553dbcf5cb52d9eb4fb1262ddf719285f353bd
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-06-29 23:07:57 +0100

    Create quickstart-chromium.sh

---
+++ quickstart-chromium.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+clear
+
+sudo apt update
+sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-install-suggests -y
+/bin/bash -c "$(curl -sL https://git.io/vokNn)"
+#Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
+sudo apt-fast install lxsession-logout policykit-1 policykit-1-gnome slim --no-install-recommends --no-install-suggests -y
+clear
+sudo apt-fast install tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox tint2 pnmixer nitrogen compton package-update-indicator cmst xinit chromium chromium-sandbox pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance lxtask pulseaudio alsa-utils dunst pavucontrol file-roller ristretto gmrun xserver-xorg xdg-user-dirs wpasupplicant --no-install-recommends --no-install-suggests -y
+xdg-user-dirs-update
+sudo apt purge --auto-remove lightdm*
+mkdir ~/.config
+mkdir ~/.config/openbox
+cp openbox/* ~/.config/openbox
+cp .conkyrc ~/
+cp .conkyrc-lua ~/
+cp .mpdconf ~/

ca2ee3a3b3eb48b5812cd8e8227e0ab8f9877e70
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2021-07-08 18:22:20 +0100

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -3,7 +3,7 @@ nitrogen --restore &
 tint2 &
 compton &
 package-update-indicator &
-cmst &
+nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 pnmixer &
 mpd &

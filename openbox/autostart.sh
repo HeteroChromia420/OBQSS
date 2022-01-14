e910a9b4f12c48b06d5020b69b5c46794dcb1fcd
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-01-14 10:13:51 +0000

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,9 +1,8 @@
 pulseaudio --start &
-nitrogen --restore &
-tint2 &
+pcmanfm --desktop &
 compton &
+xfce4-panel &
 package-update-indicator &
 nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
-pnmixer &
 mpd &

3aa5ea63752c4655cd0250cca03ccf49ff9a916b
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-01-14 16:32:26 +0000

    Add files via upload

---
+++ config/openbox/autostart.sh
@@ -0,0 +1,10 @@
+pulseaudio --start &
+pcmanfm --desktop &
+compton &
+xfce4-panel &
+package-update-indicator &
+nm-applet &
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+xfce4-power-manager &
+xscreensaver -nosplash &
+mpd &

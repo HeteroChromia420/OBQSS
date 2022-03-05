df41c1895a9db98db4b2b2dc71fc61c01eebf8cf
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-03-05 15:06:01 +0000

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -10,6 +10,7 @@ sudo apt-fast install lxsession-logout policykit-1 policykit-1-gnome --no-instal
 clear
 sudo apt-fast install xfce4-power-manager git tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox compton package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop lxpanel pnmixer arc-theme desktop-base cantata xscreensaver abiword gnumeric galculator flatpak plasma-discover plasma-discover-backend-flatpak preload --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 git clone https://github.com/dglava/arc-openbox
 mkdir ~/.themes

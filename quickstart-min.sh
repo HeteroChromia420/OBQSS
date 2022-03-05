6373177ec77ea0b2c3237a8e7c7727a89a640bee
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-03-05 19:53:00 +0000

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -6,9 +6,9 @@ sudo apt update
 sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-install-suggests -y
 /bin/bash -c "$(curl -sL https://git.io/vokNn)"
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
-sudo apt-fast install slim lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
+sudo apt-fast install lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
 clear
-sudo apt-fast install xfce4-power-manager git tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox compton package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop lxpanel pnmixer arc-theme desktop-base cantata xscreensaver abiword gnumeric galculator flatpak preload zram-tools --no-install-recommends --no-install-suggests -y
+sudo apt-fast install lightdm-gtk-greeter-settings xfce4-power-manager git tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox compton package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop lxpanel pnmixer arc-theme desktop-base cantata xscreensaver abiword gnumeric galculator flatpak preload zram-tools --no-install-recommends --no-install-suggests -y
 sudo apt-fast install --install-recommends plasma-discover plasma-discover-backend-flatpak -y
 xdg-user-dirs-update
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo

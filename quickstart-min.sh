ab1e97dc5656669059a2470fa6300a1d82dd8209
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-03-05 14:59:47 +0000

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -8,7 +8,7 @@ sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-in
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
 sudo apt-fast install lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
 clear
-sudo apt-fast install xfce4-power-manager git tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox compton package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop lxpanel pnmixer arc-theme desktop-base cantata xscreensaver abiword gnumeric galculaotr --no-install-recommends --no-install-suggests -y
+sudo apt-fast install xfce4-power-manager git tumbler smplayer mpd mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-style-plugins qt5ct openbox compton package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm mousepad lxterminal xfce4-screenshooter lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop lxpanel pnmixer arc-theme desktop-base cantata xscreensaver abiword gnumeric galculator flatpak plasma-discover plasma-discover-backend-flatpak --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 git clone https://github.com/dglava/arc-openbox

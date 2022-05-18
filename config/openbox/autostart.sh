27b942ba1e258e4834108948f690b69f65edbe95
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-18 14:19:15 +0100

    Release 8.0 Preparations

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,10 +1,9 @@
 pulseaudio --start &
 pcmanfm --desktop &
-compton &
+picom &
 xfce4-panel &
 package-update-indicator &
 nm-applet &
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 xfce4-power-manager &
 xscreensaver -nosplash &
-mpd &

a112fad13800450e290e4d5105cd0dd9820b5da2
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-19 00:04:44 +0100

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -8,7 +8,7 @@ sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-in
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
 sudo apt-fast install lightdm lxsession-logout --no-install-recommends --no-install-suggests -y
 clear
-sudo apt-fast install epiphany-browser gvfs* eject git lxappearance qt5ct qt5-gtk2-platformtheme htop obconf openbox tint2 pnmixer nitrogen network-manager network-manager-gnome xinit pcmanfm scite lxterminal pulseaudio alsa-utils dunst pavucontrol vim openbox-menu xserver-xorg xdg-user-dirs wpasupplicant preload zram-tools --no-install-recommends --no-install-suggests -y
+sudo apt-fast install epiphany-browser gvfs* eject git lxappearance qt5ct qt5-gtk2-platformtheme htop obconf openbox tint2 pnmixer nitrogen network-manager network-manager-gnome xinit pcmanfm scite lxterminal pulseaudio alsa-utils dunst pavucontrol vim openbox-menu xserver-xorg xdg-user-dirs wpasupplicant preload --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
 mkdir ~/.themes
 mkdir ~/.config

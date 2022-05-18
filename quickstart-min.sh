e2b661bc1176a3e32922345ba0cec780d834c9ee
Gil <56137935+HeteroChromia420@users.noreply.github.com>
2022-05-19 00:24:22 +0100

    Update quickstart-min.sh

--- quickstart-min.sh
+++ quickstart-min.sh
@@ -3,12 +3,10 @@
 clear
 
 sudo apt update
-sudo apt install curl aria2 wget ca-certificates --no-install-recommends --no-install-suggests -y
-/bin/bash -c "$(curl -sL https://git.io/vokNn)"
 #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
-sudo apt-fast install lightdm lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
+sudo apt install lightdm lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y
 clear
-sudo apt-fast install epiphany-browser gvfs* eject git lxappearance qt5ct qt5-gtk2-platformtheme htop obconf openbox tint2 pnmixer nitrogen network-manager network-manager-gnome xinit pcmanfm scite lxterminal pulseaudio alsa-utils dunst pavucontrol vim openbox-menu xserver-xorg xdg-user-dirs wpasupplicant preload --no-install-recommends --no-install-suggests -y
+sudo apt install epiphany-browser gvfs* eject git lxappearance qt5ct qt5-gtk2-platformtheme htop obconf openbox tint2 pnmixer nitrogen network-manager network-manager-gnome xinit pcmanfm scite lxterminal pulseaudio alsa-utils dunst pavucontrol vim openbox-menu xserver-xorg xdg-user-dirs wpasupplicant preload --no-install-recommends --no-install-suggests -y
 xdg-user-dirs-update
 mkdir ~/.themes
 mkdir ~/.config

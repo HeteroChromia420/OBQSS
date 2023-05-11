e165fe7b1ee28739a2a135892aac6959179ee92c
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-05-11 15:05:00 +0100

    Sync Repos

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,10 +1,11 @@
 pulseaudio --start &
 pcmanfm --desktop &
-picom &
+xcompmgr &
 xfce4-panel &
 package-update-indicator &
 nm-applet &
-sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+lxpolkit &
 xfce4-power-manager &
 xscreensaver -nosplash &
 flameshot &
+caffeine-indicator &
--- quickstart.sh
+++ quickstart.sh
@@ -13,9 +13,9 @@ updateSystem(){
 
 installPackages(){
     #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
-    sudo apt install lightdm lxsession-logout policykit-1 policykit-1-gnome --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
-    sudo apt install ffmpegthumbnailer lxtask lightdm-gtk-greeter-settings gimp xfce4-power-manager git tumbler celluloid mpv youtube-dl synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox picom package-update-indicator network-manager network-manager-gnome xinit epiphany-browser pcmanfm scite lxterminal flameshot lxappearance pulseaudio alsa-utils xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base quodlibet xscreensaver libreoffice papirus-icon-theme galculator flatpak preload --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
-    sudo apt install --install-recommends gnome-software gnome-software-plugin-flatpak -y | tee -a obqss-setup.log
+    sudo apt install lightdm lxsession-logout lxpolkit --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
+    sudo apt install caffeine ffmpegthumbnailer lightdm-gtk-greeter-settings xfce4-power-manager git tumbler synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox xcompmgr package-update-indicator network-manager network-manager-gnome xinit falkon pcmanfm mousepad lxterminal flameshot lxappearance pulseaudio alsa-utils dunst pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base xscreensaver papirus-icon-theme galculator flatpak preload fonts-noto* --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
+    sudo apt install --install-recommends plasma-discover plasma-discover-backend-flatpak -y | tee -a obqss-setup.log
     xdg-user-dirs-update | tee -a obqss-setup.log
     #And purge unwanted packages
     sudo apt purge --auto-remove unattended-upgrades snapd gnome-software-plugin-snap -y | tee -a obqss-setup.log

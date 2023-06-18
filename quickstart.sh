338f2921cad23a6f05ef8ee259ebf7cf7b3980b0
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-06-18 10:29:31 +0000

    We're pushing nala since it's on Deb12

--- quickstart.sh
+++ quickstart.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#EXTRATODO: Make Variants that work on other distros? What would be the implications of that? What Dependencies i'd need? what distros to focus beyond debian?
-#FIXME: Use "Nala" instead of "apt", for god's sake...
 clearScreen(){
     clear
 }
 
+#Update the system before installing anything, saves you the hedache afterwards...
 updateSystem(){
-    sudo apt update | tee -a obqss-setup.log
-    sudo apt upgrade -y | tee -a obqss-setup.log
+    sudo apt install --no-install-recommends nala | tee -a obqss-setup.log
+    sudo nala upgrade | tee -a obqss-setup.log
     clearScreen
 }
 
 installPackages(){
-    
-    #TODO: Find Alternatives to lxsession-logout that work on openbox and don't depend on systemd. 
+    #TODO: Find Alternatives to lxsession-logout that work on openbox and don't depend on systemd.
     #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
-    sudo apt install lightdm lxsession-logout lxpolkit --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
-    sudo apt install caffeine ffmpegthumbnailer lightdm-gtk-greeter-settings xfce4-power-manager git tumbler synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox xcompmgr package-update-indicator network-manager network-manager-gnome xinit falkon thunar mousepad xfce4-terminal flameshot lxappearance pulseaudio alsa-utils dunst pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base xscreensaver papirus-icon-theme galculator flatpak preload fonts-noto* --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
-    sudo apt install --install-recommends plasma-discover plasma-discover-backend-flatpak -y | tee -a obqss-setup.log
+    sudo nala install lightdm lxsession-logout policykit-1-gnome --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
+    sudo nala install caffeine ffmpegthumbnailer lightdm-gtk-greeter-settings xfce4-power-manager git tumbler synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox xcompmgr package-update-indicator network-manager network-manager-gnome xinit falkon thunar mousepad xfce4-terminal flameshot lxappearance pulseaudio alsa-utils dunst pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base xscreensaver papirus-icon-theme galculator flatpak preload fonts-noto* --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
+    sudo nala install --install-recommends gnome-software gnome-software-plugin-flatpak -y | tee -a obqss-setup.log
     xdg-user-dirs-update | tee -a obqss-setup.log
     #And purge unwanted packages
-    sudo apt purge --auto-remove unattended-upgrades snapd gnome-software-plugin-snap -y | tee -a obqss-setup.log
+    sudo nala purge --autoremove unattended-upgrades snapd gnome-software-plugin-snap -y | tee -a obqss-setup.log
     clearScreen
 }
 
@@ -41,6 +39,7 @@ gitSetup(){
     clearScreen
 }
 
+#FIXME: The error handler doesn't work properly at the moment. the log still is useful regardless.
 updateSystem && installPackages && setupFlatpak && gitSetup && echo "All components are installed successfully! Feel free to check obqss-setup.log." || echo "One or more components failed to install. Please check obqss-setup.log."
 echo "Process has completed."
 read -n 1 -s -r -p "Press any key to continue..."

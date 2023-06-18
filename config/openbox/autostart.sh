26fca0c762d2f46cc504099afa0c360602bb9ad1
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-06-18 10:32:32 +0000

    Out with the old, in with the newer...

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,10 +1,10 @@
+sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 pulseaudio --start &
 nitrogen --restore &
 xcompmgr &
 xfce4-panel &
 package-update-indicator &
 nm-applet &
-lxpolkit &
 xfce4-power-manager &
 xscreensaver -nosplash &
 flameshot &

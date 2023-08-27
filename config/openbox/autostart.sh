fbd52c2604e75772121f1dd543efe4dceaaf08b1
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-08-27 16:03:44 +0100

    Update autostart.sh

--- config/openbox/autostart.sh
+++ config/openbox/autostart.sh
@@ -1,6 +1,6 @@
 sleep 1 && /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 pulseaudio --start &
-nitrogen --restore &
+pcmanfm --desktop &
 xcompmgr &
 xfce4-panel &
 package-update-indicator &

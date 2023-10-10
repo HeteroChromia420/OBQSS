ee44a9ad669e2f1f763becf8f6f412c9d25dc521
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-10-10 22:36:37 +0100

    Add files via upload

---
+++ shared/setup-postinst.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#This script should only be used on systems with OBQSS's base system installed. this only copies the themes and configurations to the home folder of this user.
+#It also sets up flathub for that user.
+
+setupFlatpak(){
+    flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+    clear
+}
+
+gitSetup(){
+    git clone https://github.com/dglava/arc-openbox
+    mkdir ~/.themes | tee -a obqss-setup.log
+    mkdir ~/.config | tee -a obqss-setup.log
+    cp -r config/* ~/.config | tee -a obqss-setup.log
+    cp .gtkrc-2.0 ~/ | tee -a obqss-setup.log
+    cp -r arc-openbox/* ~/.themes | tee -a obqss-setup.log
+    rm -rf arc-openbox | tee -a obqss-setup.log
+    clear
+}
+ 
+setupFlatpak && gitSetup && echo "Process has been completed."
+read -n 1 -s -r -p "Press any key to continue..."

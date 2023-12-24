feca9439e677fa29ea90673f6793dc7d65a73837
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2023-12-24 18:27:12 +0000

    9.1.0

--- quickstart.sh
+++ quickstart.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
-clearScreen(){
-    clear
-}
 
 #Update the system before installing anything, saves you the hedache afterwards...
 updateSystem(){
     sudo apt upgrade | tee -a obqss-setup.log
-    clearScreen
+    clear
 }
 
 installPackages(){
     #TODO: Find Alternatives to lxsession-logout that work on openbox and don't depend on systemd.
     #Installing policykit-1-gnome first, then lxsession-logout should fix problems with devuan...
     sudo apt install lightdm lightdm-gtk lxsession-logout lxpolkit --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
-    sudo apt install xdg-desktop-portal-gtk xfce4-taskmanager caffeine ffmpegthumbnailer xfce4-power-manager git tumbler synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox xcompmgr package-update-indicator network-manager network-manager-gnome xinit falkon pcmanfm mousepad xfce4-terminal flameshot lxappearance pipewire pipewire-pulse wireplumber pamixer xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base xscreensaver papirus-icon-theme galculator flatpak preload fonts-noto* --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
+    sudo apt install xdg-desktop-portal-gtk xfce4-taskmanager caffeine ffmpegthumbnailer xfce4-power-manager git tumbler synaptic obconf lxrandr eject bash-completion gvfs* qt5-gtk2-platformtheme qt5ct openbox xcompmgr package-update-indicator network-manager network-manager-gnome xinit falkon pcmanfm mousepad xfce4-terminal flameshot lxappearance pulseaudio pamixer xfce4-notifyd pavucontrol engrampa mirage gmrun xserver-xorg xdg-user-dirs wpasupplicant htop xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin arc-theme desktop-base xscreensaver papirus-icon-theme galculator flatpak preload fonts-noto* --no-install-recommends --no-install-suggests -y | tee -a obqss-setup.log
     sudo apt install --install-recommends plasma-discover plasma-discover-backend-flatpak -y | tee -a obqss-setup.log
     xdg-user-dirs-update | tee -a obqss-setup.log
     #And purge unwanted packages
     sudo apt purge --auto-remove unattended-upgrades snapd plasma-discover-backend-snap -y | tee -a obqss-setup.log
-    clearScreen
+    clear
 }
 
 setupFlatpak(){
     flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo | tee -a obqss-setup.log
-    clearScreen
+    clear
 }
 
 gitSetup(){
@@ -36,7 +33,7 @@ gitSetup(){
     rm -rf arc-openbox | tee -a obqss-setup.log
     sudo cp -r shared /home/ | tee -a obqss-setup.log
     sudo chmod a+rwx -R /home/shared
-    clearScreen
+    clear
 }
 
 updateSystem && installPackages && setupFlatpak && gitSetup && echo "Process has been completed. Feel free to check obqss-setup.log."

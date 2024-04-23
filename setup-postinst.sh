0b0db9fcec8a52e054f2b538aab014eb6ba9170d
HeteroChromia420 <56137935+HeteroChromia420@users.noreply.github.com>
2024-04-23 20:28:40 +0100

    Update setup-postinst.sh

--- setup-postinst.sh
+++ setup-postinst.sh
@@ -9,13 +9,17 @@ setupFlatpak(){
 }
 
 gitSetup(){
-    git clone https://github.com/dglava/arc-openbox
-    mkdir ~/.themes
-    mkdir ~/.config
-    cp -r config/* ~/.config
-    cp .gtkrc-2.0 ~/
-    cp -r arc-openbox/* ~/.themes
-    rm -rf arc-openbox
+    git clone https://github.com/dglava/arc-openbox | tee -a obqss-setup.log
+    mkdir ~/.themes | tee -a obqss-setup.log
+    mkdir ~/.config | tee -a obqss-setup.log
+    cp -r config/* ~/.config | tee -a obqss-setup.log
+    cp .gtkrc-2.0 ~/ | tee -a obqss-setup.log
+    cp -r arc-openbox/* ~/.themes | tee -a obqss-setup.log
+    rm -rf arc-openbox | tee -a obqss-setup.log
+    sudo cp -r shared /home/ | tee -a obqss-setup.log
+    sudo cp -r config /home/shared | tee -a obqss-setup.log
+    sudo cp setup-postinst.sh /home/shared | tee -a obqss-setup.log
+    sudo chmod a+rx -R /home/shared | tee -a obqss-setup.log
     clear
 }
  
